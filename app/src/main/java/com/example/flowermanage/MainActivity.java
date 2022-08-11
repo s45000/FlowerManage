b78b55710717864c5c6676bfafe420eb585d09eb
allen <s45000@naver.com>
2022-08-11 16:48:55 +0900

    MediaStore.ACTION_IMAGE_CAPTURE 사용하여 사진 촬영

--- app/src/main/java/com/example/flowermanage/MainActivity.java
+++ app/src/main/java/com/example/flowermanage/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.flowermanage;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView test_view;
+    Button take_button;
+    Button upload_button;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test_view = (ImageView) findViewById(R.id.test_view);
+        take_button = (Button) findViewById(R.id.take_picture_button);
+        upload_button = (Button) findViewById(R.id.upload_picture_button);
+
+        take_button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultPicture.launch(intent);
+            }
+        });
     }
+    ActivityResultLauncher<Intent> activityResultPicture = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        bitmap = (Bitmap) extras.get("data");
+                        test_view.setImageBitmap(bitmap);
+                    }
+                }
+            }
+    );
 }
